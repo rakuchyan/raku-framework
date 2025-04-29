b1aa6884e28dc44bb69a1272bfaf8190d474b84f
LeLi <rakuchyan.le@gmail.com>
2025-04-29 18:17:33 +0800

    date

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -18,6 +18,15 @@ class Model extends \Illuminate\Database\Eloquent\Model
         return $date->format('Y-m-d H:i:s');
     }
 
+    protected function castAttribute($key, $value)
+    {
+        if (array_key_exists($key, $this->getCasts()) && in_array($this->getCasts()[$key], ['date', 'datetime'])) {
+            return is_null($value) ? null : parent::castAttribute($key, $value);
+        }
+
+        return parent::castAttribute($key, $value);
+    }
+
     protected function asJson($value)
     {
         return json_encode($value, JSON_UNESCAPED_UNICODE);
